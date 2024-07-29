49df244c17f601736710ff9a63049cfa412ed8ca
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-29 19:40:22 +0300

    Add class Customer

---
+++ src/Task6LINQ/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6LINQ
+{
+    class Customer
+    {
+        readonly int id;
+        readonly string name;
+        readonly int cityID;
+        public int ID { get { return id; } }
+        string Name { get; }
+        int CityID { get; }
+        public Customer(int id, string name, int cityID)
+        {
+            this.id = id;
+            this.name = name;
+            this.cityID = cityID;
+        }
+
+    }
+}
---
+++ src/Task6LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task6LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> customers=new List<Customer>();
+            customers.Add(new Customer(1, "Марина Мнишек", 1));
+        }
+    }
+}
