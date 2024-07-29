5bf6c48002a5f9377b6ae5c8ee82a4552ab787be
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-29 20:22:27 +0300

    Add classes City and Order

---
+++ src/Task6LINQ/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6LINQ
+{
+    class City
+    {
+        readonly int id;
+        readonly string name;
+        readonly int cityCode;
+        public int ID { get; }
+        public string Name { get; }
+        public int CityCode { get; }
+        public City(int id, string name, int cityCode)
+        {
+            this.id = id;
+            this.name = name;
+            this.cityCode = cityCode;
+        }
+    }
+}
---
+++ src/Task6LINQ/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6LINQ
+{
+    class Order
+    {
+        readonly int id;
+        readonly string customerID;
+        readonly decimal price;
+        readonly DateTime date;
+        public int ID { get; }
+        public string CustomerID { get; }
+        public decimal Price { get; }
+        public DateTime Date { get; }
+        public Order(int id, string customerID, decimal price, DateTime date)
+        {
+            this.id = id;
+            this.customerID = customerID;
+            this.price = price;
+            this.date = date;
+        }
+
+    }
+}
