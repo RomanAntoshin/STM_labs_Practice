1d32ded2fe88844475eac9e27eaec99fb236a0c1
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-08-02 15:01:22 +0300

    Add fiveth request without group

--- src/Task6LINQ/Program.cs
+++ src/Task6LINQ/Program.cs
@@ -9,28 +9,28 @@ namespace Task6LINQ
         static void Main(string[] args)
         {
             List<City> cities = new List<City>();
-            cities.Add(new City(1, "Anchorage", 907));
-            cities.Add(new City(2, "Boston", 617));
-            cities.Add(new City(3, "Chicago", 312));
-            cities.Add(new City(4, "Los Angeles", 213));
-            cities.Add(new City(5, "New York", 212));
+            cities.Add(new City(0, "Anchorage", 907));
+            cities.Add(new City(1, "Boston", 617));
+            cities.Add(new City(2, "Chicago", 312));
+            cities.Add(new City(3, "Los Angeles", 213));
+            cities.Add(new City(4, "New York", 212));
             List<Customer> customers = new List<Customer>();
-            customers.Add(new Customer(1, "Tom", cities[0]));
-            customers.Add(new Customer(2, "Ben", cities[2]));
-            customers.Add(new Customer(3, "Helen", cities[0]));
-            customers.Add(new Customer(4, "Alex", cities[3]));
-            customers.Add(new Customer(5, "Donald", cities[3]));
-            customers.Add(new Customer(6, "Gwen", cities[0]));
-            customers.Add(new Customer(7, "Helen", cities[3]));
+            customers.Add(new Customer(0, "Tom", cities[0]));
+            customers.Add(new Customer(1, "Ben", cities[2]));
+            customers.Add(new Customer(2, "Helen", cities[0]));
+            customers.Add(new Customer(3, "Alex", cities[3]));
+            customers.Add(new Customer(4, "Donald", cities[3]));
+            customers.Add(new Customer(5, "Gwen", cities[0]));
+            customers.Add(new Customer(6, "Helen", cities[3]));
             List<Order> orders = new List<Order>();
-            orders.Add(new Order(2, customers[3], 50, new DateTime(2024, 5, 1)));
-            orders.Add(new Order(3, customers[6], 91, new DateTime(2023, 11, 30)));
-            orders.Add(new Order(4, customers[3], 234, new DateTime(2024, 9, 12)));
-            orders.Add(new Order(5, customers[5], 121, new DateTime(2024, 5, 6)));
-            orders.Add(new Order(6, customers[2], 78, new DateTime(2024, 1, 9)));
-            orders.Add(new Order(1, customers[3], 150, new DateTime(2024, 5, 21)));
-            orders.Add(new Order(7, customers[2], 94, new DateTime(2024, 1, 9)));
-            orders.Add(new Order(8, customers[2], 178, new DateTime(2024, 10, 9)));
+            orders.Add(new Order(1, customers[3], 50, new DateTime(2024, 5, 1)));
+            orders.Add(new Order(2, customers[6], 91, new DateTime(2023, 11, 30)));
+            orders.Add(new Order(3, customers[3], 234, new DateTime(2024, 9, 12)));
+            orders.Add(new Order(4, customers[5], 121, new DateTime(2024, 5, 6)));
+            orders.Add(new Order(5, customers[2], 78, new DateTime(2024, 1, 9)));
+            orders.Add(new Order(0, customers[3], 150, new DateTime(2024, 5, 21)));
+            orders.Add(new Order(6, customers[2], 94, new DateTime(2024, 1, 9)));
+            orders.Add(new Order(7, customers[2], 178, new DateTime(2024, 10, 9)));
             FirstRequest(customers);
             SecondRequest(customers, orders);
             //ThirdRequest
@@ -48,8 +48,22 @@ namespace Task6LINQ
                     pairs[orders[i].Customer]++;
                 }
             }*/
-            //FourthRequest
             FourthRequest(customers, orders);
+            //FiveRequest
+            Console.WriteLine("FivethRequest");
+            var pairs = customers.ToDictionary(el => el, el => 0);
+            foreach (var customer in orders.Select(o => o.Customer))
+            {
+                if (pairs.ContainsKey(customer))
+                {
+                    pairs[customer]++;
+                }
+            }
+            var filteredPairs = pairs.Where(el => el.Value > 0);
+            foreach (var el in filteredPairs)
+            {
+                Console.WriteLine(el.Key.Name + " " + el.Value.ToString());
+            }
         }
         static void FirstRequest(List<Customer> customers)
         {
